d744e16342214badb09922daef3dec52ae909ce2
Swapnil <swapnilsagar9111@gmail.com>
2024-06-18 17:43:42 +0000

    3rd commit

--- myfile.java
+++ myfile.java
@@ -2,6 +2,6 @@ class myfile
 {
     public static void main(String []args)
     {
-        System.out.println("My First Java Program to build.");
+        System.out.println("My First Java Project to execute");
     }
 };
